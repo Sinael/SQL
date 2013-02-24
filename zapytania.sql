4c73c931a47e37ebf74c695e1423d8a26b1616f1
Grzegorz Ćwikowski <kamkra@msn.com>
2013-02-24 13:25:10 +0100

    Update zapytania.sql

--- zapytania.sql
+++ zapytania.sql
@@ -1,3 +1,23 @@
+UPDATE klient SET adres_email = 'null' where id_klient like 'KT883'- wrocenie do stanu poprzedniego
+
+ALTER TABLE producent ADD column data_zawarcia_umowy date;
+UPDATE producent SET data_zawarcia_umowy = '2010-05-01' where id_producent = 1234;
+UPDATE producent SET data_zawarcia_umowy = '2011-05-01' where id_producent = 1345;
+UPDATE producent SET data_zawarcia_umowy = '2010-09-02' where id_producent = 1567;
+UPDATE producent SET data_zawarcia_umowy = '2009-05-01' where id_producent = 8956;
+UPDATE producent SET data_zawarcia_umowy = '2011-05-01' where id_producent = 1690;
+ALTER TABLE producent DROP COLUMN data_zawarcia_umowy
+
+DROP TABLE dane,faktura,kategoria,klient,koszyk,paragon,pozycja_koszyka,pracownik,producent,przesylka,rabat,towar,zamowienie CASCADE
+
+ALTER TABLE paragon ADD COLUMN war_netto FLOAT;
+UPDATE paragon SET war_netto = wartosc * 0.77;
+ALTER TABLE paragon DROP COLUMN war_netto
+
+SELECT id_zamowienie,(SELECT imie from klient WHERE zamowienie.id_klient = klient.id_klient) AS imie_klienta, (SELECT nazwisko FROM klient WHERE zamowienie.id_klient = klient.id_klient) AS nazwisko_klient, imie, nazwisko,(SELECT wartosc FROM paragon WHERE zamowienie.id_zamowienie= paragon.id_zamowienie) AS wartosc_zrealizowana FROM zamowienie, pracownik WHERE pracownik.id_pracownik = zamowienie.id_pracownik ORDER BY imie;
+SELECT id_zamowienie,(SELECT imie from klient WHERE zamowienie.id_klient = klient.id_klient) AS imie_klienta, (SELECT nazwisko FROM klient WHERE zamowienie.id_kleint = klient.id_klient) AS nazwisko_klient, imie, nazwisko,(SELECT wartosc FROm paragon WHERE zamowienie.id_zamowienie=
+
+
 SELECT SUM(ilosc_towaru),nazwa_przedmiotu from pozycja_koszyka,towar where towar.id_towar = pozycja_koszyka.id_towar group by nazwa_przedmiotu -- Zestawienie sprzeda?y produkt?w
 
 SELECT nazwa_przedmiotu,(dostepna_ilosc -ilosc_towaru) AS D from pozycja_koszyka,towar where towar.id_towar = pozycja_koszyka.id_towar group by id_pozycja_koszyka,nazwa_przedmiotu,dostepna_ilosc - Ilo?? dost?pnych towar?w
